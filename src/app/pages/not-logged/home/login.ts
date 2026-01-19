cd8188a20cf9a55fbf798b96a160e30505ce014b
José Lucas <joselucas1298@gmail.com>
2026-01-19 20:36:26 -0300

    criado os logos e ico do site

--- src/app/pages/not-logged/home/login.ts
+++ src/app/pages/not-logged/home/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Header } from "../../../components/not-logged/header/header";
-import { SignIn } from "../../../components/not-logged/sign-in/sign-in";
+import { SignIn } from '../../../components/not-logged/sign-in/sign-in';
 
 @Component({
   selector: 'app-login',
